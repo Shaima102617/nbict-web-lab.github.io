9ebfdcbbd65d4f9a6ad0ef254b43a0af01d686af
SHAIMA <75718297+Shaima102617@users.noreply.github.com>
2021-01-10 17:23:56 +0600

    Jan10.0

---
+++ script.js
@@ -0,0 +1,7 @@
+
+        let js = "amazing";
+        if (js === "amazing") alert("Javascript is awesome!");
+        
+        console.log(2 + 6 + 8 - 13);
+        console.log("I am Sohana Shaima");
+    
