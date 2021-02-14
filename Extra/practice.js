acd68b05a62bb25510b08ae2ba3e559480af8eaf
SHAIMA <75718297+Shaima102617@users.noreply.github.com>
2021-02-14 17:39:24 +0600

    js fundamentals first class

---
+++ Extra/practice.js
@@ -0,0 +1,14 @@
+//This is NBICT LAB's code
+
+let js = "I'm awesome";
+console.log(js);
+console.log(7-11+19*2);
+
+let firstName = 'sohana ';
+let lastName ='shaima';
+console.log(firstName + ' '+ lastName + ' is a computer engineer.'
+ + lastName +' is a beautiful girl');
+
+let x;
+console.log(typeof(x));
+
