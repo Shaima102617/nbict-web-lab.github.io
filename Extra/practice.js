d9732ce89f548a3b626da27d52061725c9d2c574
SHAIMA <75718297+Shaima102617@users.noreply.github.com>
2021-02-16 18:07:54 +0600

    feb16.0

--- Extra/practice.js
+++ Extra/practice.js
@@ -1,12 +1,12 @@
 //This is NBICT LAB's code
 
-let js = "I'm awesome";
+/*let js = "I'm awesome";
 console.log(js);
 console.log(7-11+19*2);
 
 let firstName = 'sohana ';
 let lastName ='shaima';
-console.log(firstName + ' '+ lastName + ' is a computer engineer.'
+console.log(firstName +  '+ lastName + ' is a computer engineer.'
  + lastName +' is a beautiful girl');
 
 let x;
@@ -25,3 +25,78 @@ console.log(typeof(isIsland));
 console.log(typeof(population));
 console.log(typeof(country));
 console.log(typeof(language));
+*/
+/*
+let age=30;
+console.log(age);
+
+const birthYear = 1990;
+// birthYear = 1994;
+console.log(birthYear);
+
+let myName;
+// const yourName;
+ var mySubject ='CSE';
+ console.log(mySubject);
+ myName ='Shaima';
+ console.log(myName);
+ */
+//mathmatical operators
+/*
+const now = 2021;
+const ShaimaAge = now - 1993;
+const royAge = now - 2005;
+
+console.log(ShaimaAge, royAge);
+console.log(ShaimaAge * 2 , ShaimaAge / 10 ,  2 ** 3);
+
+const firstName = 'NBICT';
+const lastName = 'LAB';
+console.log(firstName + ' ' + lastName);
+
+// assignment operators
+let x = 7+11;
+x += 3; //x = x +3;
+x -= 4; // x = x-4;
+x++; //x = x+1;
+x--; //x = x-1;
+x--; //x = x-1;
+console.log(x);
+
+//comparison operators
+console.log(ShaimaAge > royAge);
+console.log(royAge>= 18);
+*/
+
+
+const country = 'Bangladesh';
+const continent ='asia';
+let population = 180;
+console.log(country , continent , population);
+
+
+const isIsland = true;
+const language = 'Bangla';
+
+
+console.log(isIsland , language);
+console.log(typeof(isIsland));
+console.log(typeof(population));
+console.log(typeof(country));
+console.log(typeof(language));
+
+ const firstHalf = population / 2;
+ const secondHalf = population / 2;
+ console.log(firstHalf , secondHalf);
+ console.log(++population);
+
+ let finpopulation = 6;
+ console.log(population > finpopulation);
+
+
+ let averagepopulation = 33; 
+ console.log(averagepopulation > population);
+
+
+let description = country + ' ' + ' is in ' + ' ' + continent + ', and it  is ' + population + ' ' + ' people speak ' + language + '.';
+console.log(description);
